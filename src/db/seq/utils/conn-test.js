044dd0d87cbe7bbf20a3c9f55644d19023c5220b
oreo <Tang.Li5@geely.com>
2023-02-13 16:13:07 +0800

    feat: add sequelize(Node.js ORM tool)

---
+++ src/db/seq/utils/conn-test.js
@@ -0,0 +1,19 @@
+/**
+ * @description 数据量连接测试
+ * @author Gary Li
+ */
+
+const seq = require('../seq');
+
+// 测试连接
+seq
+  .authenticate()
+  .then(() => {
+    console.log('ok');
+  })
+  .catch(() => {
+    console.log('fail');
+  })
+  .finally(() => {
+    process.exit();
+  });
