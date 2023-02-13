742dbd5228da461e2730de9a28c17db3cbfbcf19
oreo <Tang.Li5@geely.com>
2023-02-13 12:45:51 +0800

    feat: add mysql2

---
+++ src/config/envs/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // mysql 连接配置
+  mysqlConf: {
+    host: 'localhost',
+    user: 'root',
+    password: 'litang0530',
+    port: '3306',
+    database: 'mobilewebpage-generator',
+  },
+};
